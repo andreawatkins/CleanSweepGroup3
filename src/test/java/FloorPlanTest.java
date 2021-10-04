f3694c694688ef834999e52ca9ecec303df64ccd
Christian Kleinvehn <ckleinvehn@gmail.com>
2021-10-04 05:07:13 -0500

    Think I added JUnit correctly.

---
+++ src/test/java/FloorPlanTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class FloorPlanTest {
+    private final FloorPlan floorPlan = new FloorPlan();
+
+    @Test
+    public void testFloorPlan() {
+
+    }
+}
