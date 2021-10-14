d5be92bfb8d689776ac63b9e10125ef1b1dec69d
andreawatkins <awatki22@depaul.edu>
2021-10-14 16:51:53 -0500

    Suck up Dirt Method

--- src/main/java/CleanSweep.java
+++ src/main/java/CleanSweep.java
@@ -2,6 +2,7 @@ public class CleanSweep {
     private  int battery;
     private  int currCapacity;
     private  int totalCapacity;
+    private FloorCell currentLocation;
 
     public CleanSweep(int battery, int currCapacity, int totalCapacity) {
         this.battery = battery;
@@ -9,6 +10,16 @@ public class CleanSweep {
         this.totalCapacity = totalCapacity;
     }
 
+    public boolean needToCharge () {
+        return battery < 10;
+    }
 
+    public void suckUpDirt() {
+        while (currentLocation.dirtAmount > 0) {
+            currentLocation.dirtAmount --;
+        }
+        return;
+
+    }
 
 }
