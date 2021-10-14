369f98e4c8080ff8764e8b8ae64a82e2f8b5b1a1
andreawatkins <awatki22@depaul.edu>
2021-10-14 16:34:58 -0500

    Create Constructor & data members for CleanSweep class

--- src/main/java/CleanSweep.java
+++ src/main/java/CleanSweep.java
@@ -1,3 +1,14 @@
 public class CleanSweep {
+    private  int battery;
+    private  int currCapacity;
+    private  int totalCapacity;
+
+    public CleanSweep(int battery, int currCapacity, int totalCapacity) {
+        this.battery = battery;
+        this.currCapacity = currCapacity;
+        this.totalCapacity = totalCapacity;
+    }
+
+
 
 }
