4273cde82300e0c880b164f6a8dadbd41ecafb94
Jmacisaa-depaul <86494664+Jmacisaa-depaul@users.noreply.github.com>
2021-10-15 20:17:19 -0500

    Added basic move methods(n,s,e,w) & very basic zigzag method

--- src/main/java/CleanSweep.java
+++ src/main/java/CleanSweep.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class CleanSweep {
     //this class should have an instance of sensor simulator.
     //sensor simulator should check the current floor cell for details like surface type,
@@ -46,4 +48,93 @@ public class CleanSweep {
         }
         return cleanSweep;
     }
+
+    public void move(Direction direction) {
+        if (direction == Direction.SOUTH) {
+            moveSouth();
+        }
+
+        if (direction == Direction.EAST) {
+            moveEast();
+        }
+
+        if (direction == Direction.NORTH) {
+            moveNorth();
+        }
+
+        if (direction == Direction.WEST) {
+            moveWest();
+        }
+    }
+
+    public void moveNorth() {
+        System.out.println("Move North");
+        int x = sensors.currentLocation.x - 1;
+        int y = sensors.currentLocation.y;
+
+        sensors.currentLocation = new Location(x,y);
+        updateCurrentCell();
+    }
+
+    public void moveSouth() {
+        System.out.println("Move South");
+        int x = sensors.currentLocation.x + 1;
+        int y = sensors.currentLocation.y;
+
+        sensors.currentLocation = new Location(x,y);
+        updateCurrentCell();
+    }
+
+    public void moveEast() {
+        System.out.println("Move East");
+        int x = sensors.currentLocation.x;
+        int y = sensors.currentLocation.y + 1;
+
+        sensors.currentLocation = new Location(x,y);
+        updateCurrentCell();
+    }
+
+    public void moveWest() {
+        System.out.println("Move West");
+        int x = sensors.currentLocation.x;
+        int y = sensors.currentLocation.y - 1;
+
+        sensors.currentLocation = new Location(x,y);
+        updateCurrentCell();
+    }
+
+    public void updateCurrentCell() {
+        int x = sensors.currentLocation.x;
+        int y = sensors.currentLocation.y;
+
+        currentLocation = sensors.floorPlan.floorLayout.get(x).get(y);
+    }
+
+    public void zigZag() {
+        Direction direction = Direction.SOUTH;
+
+        while(!(sensors.isEastWall() && sensors.isSouthWall())) {
+            if(!sensors.isWall(direction)) {
+                move(direction);
+            }
+            else {
+                moveEast();
+
+                if (direction == Direction.SOUTH) {
+                    direction = Direction.NORTH;
+                }
+                else {
+                    direction = Direction.SOUTH;
+                }
+            }
+
+            suckUpDirt();
+
+            System.out.format("x: %d, y: %d\n",sensors.currentLocation.x,sensors.currentLocation.y);
+        }
+    }
+
+
+
+
 }
