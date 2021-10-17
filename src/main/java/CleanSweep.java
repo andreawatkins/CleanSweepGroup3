83301763d9a27424d34e800e26dbcb0b03eb127f
andreawatkins <awatki22@depaul.edu>
2021-10-17 17:43:29 -0500

    Test sucking up dirt and battery methods in main, update constructors !

--- src/main/java/CleanSweep.java
+++ src/main/java/CleanSweep.java
@@ -4,16 +4,21 @@ public class CleanSweep {
     //dirt level, whether there are walls to N, S, E, W, etc.) via some method like "report"
     private double battery;
     private int currCapacity;
-    private int totalCapacity;
+    private int totalCapacity = 60;
     private FloorCell currentLocation;
     private FloorCell previousLocation;
     public SensorSimulator sensors;
     public Location location; //starting location
     private CleanSweep cleanSweep = null;
+    public State currentState;
 
-    public CleanSweep(SensorSimulator sensors, Location location) {
+
+    public CleanSweep(Double battery, int currCapacity, SensorSimulator sensors, FloorCell currentLocation, FloorCell previousLocation) {
         this.sensors = sensors;
-        this.location = location;
+        this.currentLocation = currentLocation;
+        this.previousLocation = previousLocation;
+        this.battery = battery;
+        this.currCapacity = currCapacity;
 
     }
 
@@ -22,51 +27,16 @@ public class CleanSweep {
         if (cleanSweep == null) {
             synchronized (CleanSweep.class) {
                 if (cleanSweep == null) {
-                    cleanSweep = new CleanSweep(sensors, location);
+                    cleanSweep = new CleanSweep(battery, currCapacity, sensors, currentLocation, previousLocation);
                 }
             }
         }
         return cleanSweep;
     }
 
-    public CleanSweep(int battery, int currCapacity, int totalCapacity) {
-        this.battery = battery;
-        this.currCapacity = currCapacity;
-        this.totalCapacity = totalCapacity;
-    }
 
-    public boolean needToCharge () {
-        if( battery < .20*250) {
-            System.out.println("Need to charge! Low Battery!");
-            return true;
-        }
-        else return false;
-    }
-
-    public void suckUpDirt() {
-        while (currentLocation.dirtAmount > 0) {
-            currentLocation.dirtAmount --;
-            System.out.println(currentLocation.dirtAmount);
-        }
-        System.out.println("All Clean!");
-
-    }
-    /*The Clean Sweep has a limited battery life of 250 units of charge. Each movement and vacuum
-     operation requires units of charge depending on the surfaces being traversed:
-     • Bare floor - 1 unit • Low-pile carpet - 2 units • High-pile carpet - 3 units
-     The charge required for the Clean Sweep to move from location A to location B is the average
-     of the required charge costs for the surfaces at the two locations.
-     For example, if the Clean Sweep moves from a bare floor to low-pile carpet, then the charge required is 1.5 units.
-     It costs the same amount of charge to clean the current location is it does to traverse that location.
-     The Clean Sweep should always return to its charging station before it runs out of power.
-     Homeowners that come home and discover their robotic vacuum out of power in the middle of a room
-     tend to be dissatisfied customers.
-     As it returns to its charging station, the Clean Sweep will not perform any cleaning.
-     Upon returning to its charging station, the Clean Sweep will automatically re-charge to full capacity.
-     Once re-charged, it will resume cleaning until it detects that it has visited every accessible location
-     on the current floor.*/
-    public double useBattery(){
-        double batteryDec =0;
+    public double useBattery() {
+        double batteryDec = 0;
 
         if (currentLocation.surfaceType == SurfaceType.BARE_FLOOR)
             batteryDec = 1;
@@ -75,30 +45,50 @@ public class CleanSweep {
         else if (currentLocation.surfaceType == SurfaceType.HIGH_PILE_CARPET)
             batteryDec = 3;
 
-        if(previousLocation==currentLocation) {
-            this.battery = this.battery - batteryDec;
-            return battery;
+        if (previousLocation == currentLocation) {
+            battery = battery - batteryDec;
         }
 
-        else{
-            if((currentLocation.surfaceType == SurfaceType.BARE_FLOOR) && (previousLocation.surfaceType == SurfaceType.LOW_PILE_CARPET))
-                this.battery = this.battery - ((batteryDec + 2) /2);
-            if((currentLocation.surfaceType == SurfaceType.BARE_FLOOR) && (previousLocation.surfaceType == SurfaceType.HIGH_PILE_CARPET))
-                this.battery = this.battery - ((batteryDec + 3)/2);
-            if((currentLocation.surfaceType == SurfaceType.LOW_PILE_CARPET) && (previousLocation.surfaceType == SurfaceType.BARE_FLOOR))
-                this.battery = this.battery - ((batteryDec + 1) /2);
-            if((currentLocation.surfaceType == SurfaceType.LOW_PILE_CARPET) && (previousLocation.surfaceType == SurfaceType.HIGH_PILE_CARPET))
-                this.battery = this.battery - ((batteryDec + 3) /2);
-            if((currentLocation.surfaceType == SurfaceType.HIGH_PILE_CARPET) && (previousLocation.surfaceType == SurfaceType.BARE_FLOOR))
-                this.battery = this.battery - ((batteryDec + 1) /2);
-            if((currentLocation.surfaceType == SurfaceType.HIGH_PILE_CARPET) && (previousLocation.surfaceType == SurfaceType.LOW_PILE_CARPET))
-                this.battery = this.battery - ((batteryDec + 2) /2);
-            return battery;
+        else {
+            if ((currentLocation.surfaceType == SurfaceType.BARE_FLOOR) && (previousLocation.surfaceType == SurfaceType.LOW_PILE_CARPET))
+                battery = battery - ((batteryDec + 2) / 2);
+            if ((currentLocation.surfaceType == SurfaceType.BARE_FLOOR) && (previousLocation.surfaceType == SurfaceType.HIGH_PILE_CARPET))
+                battery = battery - ((batteryDec + 3) / 2);
+            if ((currentLocation.surfaceType == SurfaceType.LOW_PILE_CARPET) && (previousLocation.surfaceType == SurfaceType.BARE_FLOOR))
+                battery = battery - ((batteryDec + 1) / 2);
+            if ((currentLocation.surfaceType == SurfaceType.LOW_PILE_CARPET) && (previousLocation.surfaceType == SurfaceType.HIGH_PILE_CARPET))
+                battery = battery - ((batteryDec + 3) / 2);
+            if ((currentLocation.surfaceType == SurfaceType.HIGH_PILE_CARPET) && (previousLocation.surfaceType == SurfaceType.BARE_FLOOR))
+                battery = battery - ((batteryDec + 1) / 2);
+            if ((currentLocation.surfaceType == SurfaceType.HIGH_PILE_CARPET) && (previousLocation.surfaceType == SurfaceType.LOW_PILE_CARPET))
+                battery = battery - ((batteryDec + 2) / 2);
 
 
 
         }
+
+        if (needToCharge()) {System.out.println(this.currentState); return battery;}
+        else return battery;
     }
 
+    public boolean needToCharge() {
+        if (battery < .20 * 250) {
+            this.currentState = State.LOW_BATTERY;
+            System.out.println("Need to charge! Low Battery!");
 
+            return true;
+        } else return false;
+    }
+
+    public void suckUpDirt() {
+        while (currentLocation.dirtAmount > 0) {
+            currentLocation.dirtAmount--;
+            currCapacity ++;
+            System.out.println("Cleaning... " + previousLocation.dirtAmount + " units of dirt left");
+        }
+
+        System.out.println("FloorCell is Clean!");
+        System.out.println("Capacity at : "  + currCapacity + " units");
+
+    }
 }
