30ffbbe6763ce2e041d8612d9186ae2edc008864
andreawatkins <awatki22@depaul.edu>
2021-10-29 16:35:38 -0500

    Added CleanSweep Test class

---
+++ src/test/java/CleanSweepTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+public class CleanSweepTest {
+
+    @Test
+    public void batteryDecTest(){
+
+    }
+
+    @Test
+    public void lowBatteryTest(){
+
+    }
+
+    @Test
+    public void cleanDirtTest(){
+
+    }
+
+    @Test
+    public void capacityDecTest(){
+
+    }
+
+    @Test
+    public void fullCapacityTest(){
+
+    }
+
+
+
+}
